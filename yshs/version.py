6bb72ab08cb36853da0cdb73a8175041578eec0c
zfy0216 <zhangfy0216@gmail.com>
2023-11-09 22:38:43 +0800

    zzd

--- yshs/version.py
+++ yshs/version.py
@@ -1,5 +1,5 @@
 __appname__ = 'yshs'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __version_suffix__ = 'Beta'  # version suffix, like: 1.0.0-Dev, Beta, Stable
 __author__ = 'Feiyan Zhang'
 __email__ = 'zhangfy0216@gmail.cpm'
