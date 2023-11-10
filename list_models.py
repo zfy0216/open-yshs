fd9bcb919ff7977fa4743f0177453c582adddcd0
zfy0216 <zhangfy0216@gmail.com>
2023-11-10 11:48:17 +0800

    zfy

--- list_models.py
+++ list_models.py
@@ -1,10 +1,7 @@
 
-
-
 import os, sys
 import yshs
 yshs.api_key = os.getenv('YSHS_API_KEY')
 
 response = yshs.Models.list(refresh=True, return_all_info=True)
 print(response)
-
--- request_model.py
+++ request_model.py
@@ -4,15 +4,16 @@ import yshs
 yshs.api_key = os.getenv('YSHS_API_KEY')
 
 
-def request_model():
+def request_model(model, system_prompt, prompt, **kwargs):
     responese = yshs.LLM.chat(
-        model="openai/gpt-3.5-turbo",  # 选择模型
+        model=model,  # 选择模型
         messages=[
-          {"role": "system", "content": "You are a helpful assistant."},  # 系统提示
-          {"role": "user", "content": "Who won the world series in 2020?"},  # 第一个问题
+          {"role": "system", "content": system_prompt},  # 系统提示
+          {"role": "user", "content": prompt},  # 第一个问题
           # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},  # 第一个的答案
           # {"role": "user", "content": "Where was it played?"}  # 第二个问题
-      ]
+        ],
+        **kwargs,
     )
 
     full_response = ""
@@ -23,5 +24,18 @@ def request_model():
     print()
     return full_response
 
-answer = request_model()
+def request_gpt35():
+    model = "openai/gpt-3.5-turbo"
+    system_prompt = "You are a helpful assistant."
+    prompt = "Who won the world series in 2020?"
+    return request_model(model, system_prompt, prompt)
+
+def request_gpt4():
+    model = "openai/gpt-4"
+    system_prompt = "You are a helpful assistant."
+    prompt = "Who won the world series in 2020?"
+    return request_model(model, system_prompt, prompt)
+
+answer = request_gpt35()
 # print(answer)
+# answer = request_gpt4()
